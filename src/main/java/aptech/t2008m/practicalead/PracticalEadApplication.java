33028507642fbc33ae54468e8023e21d82c338e8
Huy Quang <truongquanghuybx0914@gmail.com>
2022-05-27 09:15:17 +0700

    Initial commit

---
+++ src/main/java/aptech/t2008m/practicalead/PracticalEadApplication.java
@@ -0,0 +1,13 @@
+package aptech.t2008m.practicalead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalEadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalEadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aptech/t2008m/practicalead/PracticalEadApplicationTests.java
@@ -0,0 +1,13 @@
+package aptech.t2008m.practicalead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalEadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
